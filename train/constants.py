5bdd17c1af707078e6133e89f452a1c67533af3b
Anna Nikiforovskaya <nikianna@devfair0292.h2.fair>
2019-07-23 09:15:59 -0700

    made better subprocess calling, constants separated

---
+++ train/constants.py
@@ -0,0 +1,5 @@
+import os
+from os import path
+
+SRC_DIR = path.abspath(path.join(path.dirname(__file__), os.pardir))
+PANTHEON_ROOT = path.join('_build', 'deps', 'pantheon')
--- train/remoteenv.py
+++ train/remoteenv.py
@@ -3,13 +3,14 @@
 import os
 from os import path
 import argparse
-import multiprocessing as mp
 import threading
 import time
 import subprocess
 
 import numpy as np
 import sys
+
+from constants import SRC_DIR, PANTHEON_ROOT
 
 
 parser = argparse.ArgumentParser(description='Remote Environment Server')
@@ -19,30 +20,21 @@ parser.add_argument('--start_port', default=60000, type=int, metavar='P',
 parser.add_argument('--num_servers', default=4, type=int, metavar='N',
                     help='Number of environment servers.')
 
-src_dir = path.abspath(path.dirname(__file__))
-test_path = path.join(src_dir, '_build', 'deps',
-                    'pantheon', 'src', 'experiments', 'test.py')
+test_path = path.join(SRC_DIR, PANTHEON_ROOT, 'src', 'experiments', 'test.py')
 
 
-def create_env(port, lock=threading.Lock()):
+def get_args(port):
     cmd = [test_path, 'local', '--schemes', 'mvfst_rl', '--extra_sender_args',
                     '--cc_env_port=%d --cc_env_mode=train' % port]
-    return subprocess.call(cmd)
+    return cmd
 
 if __name__ == "__main__":
     flags = parser.parse_args()
 
     processes = []
     for i in range(flags.num_servers):
-        p = mp.Process(
-            target=create_env, args=((flags.start_port + i),), daemon=True
-        )
-        p.start()
+        p = subprocess.Popen(get_args(flags.start_port + i))
         processes.append(p)
 
-    try:
-        # We are only here to listen to the interrupt.
-        while True:
-            time.sleep(10)
-    except KeyboardInterrupt:
-        pass
+    for p in processes:
+        p.wait()
