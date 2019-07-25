590affe8f278549c2cd566bc83f5a0f46895b486
Anna Nikiforovskaya <nikianna@devfair0292.h2.fair>
2019-07-25 08:31:44 -0700

    now test.py instances can be different with setup.yml

---
+++ train/helpers/utils.py
@@ -0,0 +1,47 @@
+import sys
+from os import path
+import yaml
+import time
+import string
+import itertools
+from datetime import datetime
+from collections import defaultdict
+
+from constants import SRC_DIR
+
+def parse_setup():
+    with open(path.join(SRC_DIR, 'train/setup.yml')) as cfg:
+        return yaml.load(cfg)
+
+
+expt_cfg = parse_setup()
+meta = expt_cfg['meta']
+
+
+class SafeDict(dict):
+    def __missing__(self, key):
+        return '{' + key + '}'
+
+
+# format 'format_string' but ignore keys that do not exist in 'key_dict'
+def safe_format(format_string, key_dict):
+    return string.Formatter().vformat(format_string, (), SafeDict(key_dict))
+
+
+def expand_matrix(matrix_cfg):
+    input_list = []
+    for variable, value_list in matrix_cfg.items():
+        input_list.append([{variable:value} for value in value_list])
+
+    ret = []
+    for element in itertools.product(*input_list):
+        tmp = {}
+        for kv in element:
+            tmp.update(kv)
+        ret.append(tmp)
+
+    return ret
+
+
+def utc_date():
+    return datetime.utcnow().strftime('%Y-%m-%dT%H-%M')
--- train/remoteenv.py
+++ train/remoteenv.py
@@ -6,12 +6,13 @@ import argparse
 import threading
 import time
 import subprocess
+import shlex
 
 import numpy as np
 import sys
 
 from constants import SRC_DIR, PANTHEON_ROOT
-
+from helpers import utils
 
 parser = argparse.ArgumentParser(description='Remote Environment Server')
 
@@ -22,19 +23,42 @@ parser.add_argument('--num_servers', default=4, type=int, metavar='N',
 
 test_path = path.join(PANTHEON_ROOT, 'src/experiments/test.py')
 
+def run_emu(flags):
+    sys.stderr.write('----- Running emulation experiments -----\n')
 
-def get_cmd(port):
-    cmd = [test_path, 'local', '--schemes', 'mvfst_rl',
-                    '--extra_sender_args', '--cc_env_port=%d --cc_env_mode=train' % port]
+    cfg = utils.expt_cfg['emu']
+    matrix = utils.expand_matrix(cfg['matrix'])
+
+    # create a queue of jobs
+    job_queue = []
+    for mat_dict in matrix:
+        for job_cfg in cfg['jobs']:
+            cmd_tmpl = job_cfg['command']
+
+            # 1. expand macros
+            cmd_tmpl = utils.safe_format(cmd_tmpl, cfg['macros'])
+            # 2. expand variables in mat_dict
+            cmd_tmpl = utils.safe_format(cmd_tmpl, mat_dict)
+            # 3. expand meta
+            cmd_tmpl = utils.safe_format(cmd_tmpl, utils.meta)
+            cmd_tmpl = utils.safe_format(cmd_tmpl, {'src_dir' : SRC_DIR})
+
+            job_queue.append((job_cfg, cmd_tmpl))
+
+    processes = []
+    for i in range(min(flags.num_servers, len(job_queue))):
+        job_cfg, cmd = job_queue[i]
+        with open(path.join(SRC_DIR, "sc_%d.log" % job_cfg['scenario']), 'w') as log_f:
+            p = subprocess.Popen(get_cmd(cmd, flags.start_port + i), stdout=log_f, stderr=log_f)
+    for p in processes:
+        p.wait()
+
+def get_cmd(cmd, port):
+    cmd = shlex.split(cmd) + ['--extra_sender_args', '--cc_env_port=%d --cc_env_mode=train' % port]
+    cmd[0] = path.abspath(cmd[0])
+    print(cmd)
     return cmd
 
 if __name__ == "__main__":
     flags = parser.parse_args()
-
-    processes = []
-    for i in range(flags.num_servers):
-        p = subprocess.Popen(get_cmd(flags.start_port + i))
-        processes.append(p)
-
-    for p in processes:
-        p.wait()
+    run_emu(flags)
