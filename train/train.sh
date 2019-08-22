b29fcdca748d67299851fc20448237ffd7a86838
Viswanath Sivakumar <viswanath@fb.com>
2019-08-22 02:27:39 -0700

    Fix passing job_ids to test.sh

--- train/train.sh
+++ train/train.sh
@@ -88,6 +88,9 @@ kill -9 "$BG_PID"
 pkill -9 -f "pantheon"
 
 echo "Testing..."
-"$ROOT_DIR"/train/test.sh --checkpoint "$CHECKPOINT"
+"$ROOT_DIR"/train/test.sh \
+  --checkpoint "$CHECKPOINT" \
+  --max_jobs "$MAX_JOBS" \
+  --job_ids "$JOB_IDS"
 
 echo "All done! Model: $CHECKPOINT."
